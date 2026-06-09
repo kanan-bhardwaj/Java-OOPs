3648a7ee1dccb98a24a903ec7155aebe5d2aec57
Kanan Bhardwaj <bhardwajkanan1013@gmail.com>
2026-06-09 11:23:25 -0700

    Create InterfacesDemo.java

---
+++ 11-Interfaces/InterfacesDemo.java
@@ -0,0 +1,16 @@
+interface Vehicle {
+    void start();
+}
+
+class Car implements Vehicle {
+    public void start() {
+        System.out.println("Car started");
+    }
+}
+
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        Vehicle v = new Car();
+        v.start();
+    }
+}
